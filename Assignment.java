f189e774f571e7b78d18a204fa8ff889fb4083ce
aininjeffery <aininjeffery@gmail.com>
2026-05-09 01:37:48 +0800

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,1 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
 
+// ================= MEMBER 4 =================
+// Contributed by: Nur Ainin Sofiya binti Jeffery
+// Role: GUI Framework & Integration
+// - Create the main class and the main(String[] args) entry point
+// - Code the Encode Button to fetch input, run Member 1-3's method, and update the UI.
